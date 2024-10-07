90d5e6bd521edd1524c7ddf7155e48581893d8c3
Hatem <hbasheer@live.com>
2024-10-08 01:45:26 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, inject, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'todo';
-}
+export class AppComponent {}
